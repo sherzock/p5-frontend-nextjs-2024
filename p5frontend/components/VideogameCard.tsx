825f9bd5e415a7536be64270b8f10b2892c264c0
sherzock <jacobo.galofre@gmail.com>
2024-06-20 20:51:33 +0200

    Less info on game card

--- p5frontend/components/VideogameCard.tsx
+++ p5frontend/components/VideogameCard.tsx
@@ -11,11 +11,8 @@ export default function VideogameCard({ videogame }: { videogame: Videogame }) {
         <div className="m-5 flex flex-col justify-around text-center">
           <p className="italic">{videogame.name}</p>
           <Image src={videogame.poster} width={86} height={80} alt={`Poster of "${videogame.name}"`} className="w-80 h-120 mt-5 mb-5"/>
-          <p className="italic">Developer: {videogame.developer}</p>
-          <p className="">Description: {videogame.description}</p>
-          <p className="bold">Achivements: {videogame.totalAchievements}</p>
-          <p className="">Since: {videogame.launchYear}</p>
-          <p className="bold">Reviews: {videogame.grades}/10</p> 
+          <p className="m-5">{videogame.description}</p>
+          <p className="bold">Reviews {videogame.grades} / 10</p> 
         </div>
       </div>
   );
