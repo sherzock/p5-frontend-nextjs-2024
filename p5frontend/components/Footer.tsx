b4f078dcb774e5c7e06621c96e387b448be12dcd
sherzock <jacobo.galofre@gmail.com>
2024-06-23 13:31:02 +0200

    Changed create game position style

--- p5frontend/components/Footer.tsx
+++ p5frontend/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className=" h-8 text-xs text-center pt-1 ">
+      <footer className=" h-8 text-xs text-center pt-1">
         <div className="m-5">
           <a href="https://github.com/sherzock" target="_blank">
             Jacobo Galofre Clabetó
--- p5frontend/components/Header.tsx
+++ p5frontend/components/Header.tsx
@@ -3,13 +3,13 @@ import Button from "./ui/Button";
 
 export default function Header() {
   return (
-    <header className="border-b-2 border-white border-solid flex flex-row place-items-center justify-center		">
+    <header className="border-b-2 border-white border-solid flex flex-row place-items-center justify-center	">
       <Link href="/" className="mb-5">
-        <div id="logo" className="text-center font-extrabold text-5xl text-white m-5">
+        <div id="logo" className="text-center font-extrabold text-5xl text-white m-5	">
             Library
         </div>
       </Link>
-      <Link href={"/create"} className="justify-self-end	">
+      <Link href={"/create"} className=" absolute	right-10	">
         <Button label="Create Game"></Button>
       </Link>
     </header>
